f1fa2ff198c97742b398c69dd180f0e16b96fe97
maru0210 <m.ryuto0210@gmail.com>
2025-01-04 03:11:12 +0900

    Add font: JetBrainsMono

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from "tailwindcss/defaultTheme"
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        logo: ['Outfit', ...defaultTheme.fontFamily.sans],
-        body: ['MPLUS2', ...defaultTheme.fontFamily.sans],
+        logo: ["Outfit", ...defaultTheme.fontFamily.sans],
+        body: ["MPLUS2", ...defaultTheme.fontFamily.sans],
+        code: ["JetBrainsMono", ...defaultTheme.fontFamily.sans],
       },
     },
   },
